ee88e07479a69f0609d0bc37c072d1011bdaacce
AR Gorsi <argorsi707@gmail.com>
2024-02-29 15:51:19 +0500

    created contact page

--- main.js
+++ main.js
@@ -2,4 +2,6 @@ import "./styles/modern-normalize.css";
 import "./styles/components/header.css";
 import "./styles/components/hero.css";
 import "./styles/components/aboutme.css";
+import "./styles/components/projects.css";
+import "./styles/components/contact.css";
 import "./styles/global.css";
