fa619264a904ff955a77d77712d9eeadd8535216
AR Gorsi <argorsi707@gmail.com>
2024-02-29 13:52:58 +0500

    created aboutme section and made it responsive

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import "./styles/modern-normalize.css";
 import "./styles/components/header.css";
 import "./styles/components/hero.css";
+import "./styles/components/aboutme.css";
 import "./styles/global.css";
